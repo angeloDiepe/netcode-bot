88c9422ad53249a0c6d944a7149624eeff9b43cc
angeloDiepe <angelodiepe10@yahoo.com>
2019-11-12 21:04:38 +0100

    first discord bot

--- netcode-bot.js
+++ netcode-bot.js
@@ -3,20 +3,8 @@ const token = process.env.TOKEN;
 
 const client = new Discord.Client();
 
-console.log("HEllo")
 client.on('ready',()=>{
-	console.log('Bot is now connected');
+	console.log('Bot is now connected',token);
 	// client.channels.find(x=>x.name === 'test').send('hello im now connected')
 })
-client.on('guildMemberAdd', member => {
-  // Send the message to a designated channel on a server:
-  const channel = member.guild.channels.find(ch => ch.name === 'member-log');
-  // Do nothing if the channel wasn't found on this server
-  if (!channel) return;
-  // Send the message, mentioning the member
-  channel.send(`Welcome to the NetCode server😁, ${member}`);
-});
-client.on('message', (msg)=>{
-	console.log(msg)
-});
 client.login(token);
